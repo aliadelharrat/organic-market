ec2fbb4e524bbd47ba938e49546caddd67f3cd43
Adel Harrat <a03l.web@gmail.com>
2025-05-16 10:44:42 +0100

    Removed | from title, Added slugify

--- index.js
+++ index.js
@@ -2,24 +2,32 @@ const http = require("http");
 const url = require("url");
 const fs = require("fs");
 const replaceTemplate = require("./modules/replaceTemplate");
+const slugify = require("slugify");
 
 const port = 8000;
 
 const tempOverview = fs.readFileSync(
-  __dirname + "/templates/products.html",
+  `${__dirname}/templates/products.html`,
   "utf-8"
 );
+
 const tempProduct = fs.readFileSync(
-  __dirname + "/templates/product.html",
+  `${__dirname}/templates/product.html`,
   "utf-8"
 );
+
 const tempProductCard = fs.readFileSync(
-  __dirname + "/templates/product-card.html",
+  `${__dirname}/templates/product-card.html`,
   "utf-8"
 );
 
 const data = fs.readFileSync(__dirname + "/data.json", "utf-8");
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+
+console.log(slugify("Fresh Avocados", { lower: true }));
 
 const server = http.createServer(function (req, res) {
   const { query, pathname } = url.parse(req.url, true);
